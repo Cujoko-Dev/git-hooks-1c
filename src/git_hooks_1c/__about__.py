dd63e9b1c9ced8e09e9d98d89e44375cb0d02774
Cujoko <cujoko@gmail.com>
2025-07-03 05:55:20 +0900

    Отформатирует

--- src/git_hooks_1c/__about__.py
+++ src/git_hooks_1c/__about__.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-
-"""Информация о пакете"""
-
-
 import importlib.metadata
 
 __version__ = importlib.metadata.version(__package__)
--- src/git_hooks_1c/__init__.py
+++ src/git_hooks_1c/__init__.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-
-"""Для импорта"""
-
-
 from loguru import logger
 
 from git_hooks_1c.__about__ import __version__
