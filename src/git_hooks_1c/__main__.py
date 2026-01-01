cd6486e25724d11bc1c78eaaf52a9c9db1ed32f1
Cujoko <cujoko@gmail.com>
2026-01-01 21:20:11 +0900

    Обновит __main__.py

--- src/git_hooks_1c/__main__.py
+++ src/git_hooks_1c/__main__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from git_hooks_1c.core import run
 
-sys.path.insert(0, Path(__file__).parent.parent)
+sys.path.insert(0, str(Path(__file__).parent.parent))
 
 if __name__ == "__main__":
     run()
